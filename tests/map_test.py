4343ad2d0f808c1314446976f4608892b9bb5a71
Can Gencer <cgencer@gmail.com>
2015-12-10 18:12:49 +0100

    add simple tests for map

---
+++ tests/map_test.py
@@ -0,0 +1,48 @@
+import unittest
+import hazelcast
+from util import random_string
+
+class ClientMapTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        config = hazelcast.Config()
+        config.addresses.append("127.0.0.1:5701")
+        cls.client = hazelcast.HazelcastClient(config)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.client.shutdown()
+
+    def setUp(self):
+        self.map = self.client.get_map(random_string())
+
+    def tearDown(self):
+        self.map.destroy()
+
+    def fill_map(self, map):
+        for i in xrange(0, 10):
+            map.put("key-%d" % i, "value-%d" %i)
+
+    def test_put_get(self):
+        self.fill_map(self.map)
+        for i in xrange(0, 10):
+            self.assertEqual("value-%d" %i, self.map.get("key-%d" % i))
+
+    def test_contains_key(self):
+        self.fill_map(self.map)
+
+        self.assertTrue(self.map.contains_key("key-1"))
+        self.assertFalse(self.map.contains_key("key-10"))
+
+    def test_size(self):
+        self.fill_map(self.map)
+
+        self.assertEqual(10, self.map.size())
+
+    def test_remove(self):
+        self.fill_map(self.map)
+
+        self.map.remove("key-1")
+        self.assertEqual(9, self.map.size())
+        self.assertFalse(self.map.contains_key("key-1"))
---
+++ tests/util.py
@@ -0,0 +1,5 @@
+from uuid import uuid4
+
+
+def random_string():
+    return str(uuid4())
