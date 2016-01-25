d415b37988d9a2ab2f73120abe895bfa6c3b9c1f
Can Gencer <cgencer@gmail.com>
2016-01-25 13:33:58 +0100

    add Lock proxy and tests

--- hazelcast/proxy/lock.py
+++ hazelcast/proxy/lock.py
@@ -1,31 +1,37 @@
-from hazelcast.proxy.base import Proxy
+from hazelcast.protocol.codec import lock_force_unlock_codec, lock_get_lock_count_codec, \
+    lock_get_remaining_lease_time_codec, lock_is_locked_by_current_thread_codec, lock_is_locked_codec, lock_lock_codec,\
+    lock_try_lock_codec, lock_unlock_codec
+from hazelcast.proxy.base import PartitionSpecificProxy
+from hazelcast.util import thread_id, to_millis
 
 
-class Lock(Proxy):
-
+class Lock(PartitionSpecificProxy):
     def force_unlock(self):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_force_unlock_codec, name=self.name)
 
     def get_lock_count(self):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_get_lock_count_codec, name=self.name)
 
     def get_remaining_lease_time(self):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_get_remaining_lease_time_codec, name=self.name)
 
     def is_locked(self):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_is_locked_codec, name=self.name)
 
     def is_locked_by_current_thread(self):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_is_locked_by_current_thread_codec, name=self.name,
+                                                thread_id=thread_id())
 
     def lock(self, lease_time=-1):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_lock_codec, name=self.name, lease_time=to_millis(lease_time),
+                                                thread_id=thread_id())
 
-    def try_lock(self, lease_time=-1, timeout=None):
-        raise NotImplementedError
+    def try_lock(self, timeout=0, lease_time=-1):
+        return self._encode_invoke_on_partition(lock_try_lock_codec, name=self.name, lease=to_millis(lease_time),
+                                                thread_id=thread_id(), timeout=to_millis(timeout))
 
     def unlock(self):
-        raise NotImplementedError
+        return self._encode_invoke_on_partition(lock_unlock_codec, name=self.name, thread_id=thread_id())
 
     def __str__(self):
         return "Lock(name=%s)" % self.name
---
+++ tests/proxy/lock_test.py
@@ -0,0 +1,80 @@
+from threading import Event
+
+from tests.base import SingleMemberTestCase
+from tests.util import random_string
+
+
+class ClientLockTest(SingleMemberTestCase):
+    def setUp(self):
+        self.lock = self.client.get_lock(random_string()).blocking()
+
+    def tearDown(self):
+        self.lock.destroy()
+
+    def test_lock(self):
+        self.lock.lock(10)
+
+        e = Event()
+
+        def lock():
+            if not self.lock.try_lock():
+                e.set()
+
+        self.start_new_thread(lock)
+        self.assertSetEventually(e)
+
+    def test_lock_with_lease(self):
+        self.lock.lock(lease_time=1)
+
+        e = Event()
+
+        def lock():
+            if self.lock.try_lock(timeout=10):
+                e.set()
+
+        self.start_new_thread(lock)
+        self.assertSetEventually(e)
+
+    def test_is_locked_when_not_locked(self):
+        self.assertFalse(self.lock.is_locked())
+
+    def test_is_locked_when_locked(self):
+        self.lock.lock()
+        self.assertTrue(self.lock.is_locked())
+
+    def test_is_locked_when_locked_and_unlocked(self):
+        self.lock.lock()
+        self.lock.unlock()
+        self.assertFalse(self.lock.is_locked())
+
+    def test_is_locked_by_current_thread(self):
+        self.lock.lock()
+        self.assertTrue(self.lock.is_locked_by_current_thread())
+
+    def test_is_locked_by_current_thread_with_another_thread(self):
+        self.start_new_thread(lambda: self.lock.lock())
+
+        self.assertFalse(self.lock.is_locked_by_current_thread())
+
+    def test_get_remaining_lease_time(self):
+        self.lock.lock(10)
+        self.assertGreater(self.lock.get_remaining_lease_time(), 9000)
+
+    def test_get_lock_count_single(self):
+        self.lock.lock()
+
+        self.assertEqual(self.lock.get_lock_count(), 1)
+
+    def test_get_lock_count_when_reentrant(self):
+        self.lock.lock()
+        self.lock.lock()
+
+        self.assertEqual(self.lock.get_lock_count(), 2)
+
+    def test_force_unlock(self):
+        t = self.start_new_thread(lambda: self.lock.lock())
+        t.join()
+
+        self.lock.force_unlock()
+
+        self.assertFalse(self.lock.is_locked())
