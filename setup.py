8b3c5393922b260e4f12d6dd6204480188b7f371
Asım Arslan <arslanasim@gmail.com>
2016-03-15 16:57:43 +0200

    release 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
         name='hazelcast-python-client',
-        version='0.2',
+        version='0.2.1',
         description='Hazelcast Python Client',
         long_description=long_description,
         url='https://github.com/hazelcast/hazelcast-python-client',
