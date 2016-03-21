a94ccf521e0ecb515cb449a596c7bada993fb576
Asım Arslan <arslanasim@gmail.com>
2016-03-21 13:50:12 +0200

    integer serialization tests

--- tests/serialization/serialization_test.py
+++ tests/serialization/serialization_test.py
@@ -1,9 +1,10 @@
 import unittest
 
-from hazelcast.config import SerializationConfig
+from hazelcast.config import SerializationConfig, INTEGER_TYPE
+from hazelcast.serialization.serialization_const import CONSTANT_TYPE_BYTE, CONSTANT_TYPE_SHORT, CONSTANT_TYPE_INTEGER, \
+    CONSTANT_TYPE_LONG
 from hazelcast.serialization.service import SerializationServiceV1
-
-from ..util import random_string
+from tests.util import random_string
 
 
 class SerializationTestCase(unittest.TestCase):
@@ -40,5 +41,72 @@ class SerializationTestCase(unittest.TestCase):
         self.assertEqual(16, len(data.to_bytes()))
 
 
-if __name__ == '__main__':
-    unittest.main()
+byte_val = 0x12
+short_val = 0x1234
+int_val = 0x12345678
+long_val = 0x1122334455667788
+
+
+class IntegerTestCase(unittest.TestCase):
+    def test_dynamic_case(self):
+        config = SerializationConfig()
+        config.default_integer_type = INTEGER_TYPE.VAR
+        service = SerializationServiceV1(serialization_config=config)
+
+        d1 = service.to_data(byte_val)
+        d2 = service.to_data(short_val)
+        d3 = service.to_data(int_val)
+        d4 = service.to_data(long_val)
+
+        self.assertEqual(d1.get_type(), CONSTANT_TYPE_BYTE)
+        self.assertEqual(d2.get_type(), CONSTANT_TYPE_SHORT)
+        self.assertEqual(d3.get_type(), CONSTANT_TYPE_INTEGER)
+        self.assertEqual(d4.get_type(), CONSTANT_TYPE_LONG)
+
+    def test_byte_case(self):
+        config = SerializationConfig()
+        config.default_integer_type = INTEGER_TYPE.BYTE
+        service = SerializationServiceV1(serialization_config=config)
+
+        d1 = service.to_data(byte_val)
+
+        self.assertEqual(d1.get_type(), CONSTANT_TYPE_BYTE)
+
+    def test_short_case(self):
+        config = SerializationConfig()
+        config.default_integer_type = INTEGER_TYPE.SHORT
+        service = SerializationServiceV1(serialization_config=config)
+
+        d1 = service.to_data(byte_val)
+        d2 = service.to_data(short_val)
+
+        self.assertEqual(d1.get_type(), CONSTANT_TYPE_SHORT)
+        self.assertEqual(d2.get_type(), CONSTANT_TYPE_SHORT)
+
+    def test_int_case(self):
+        config = SerializationConfig()
+        config.default_integer_type = INTEGER_TYPE.INT
+        service = SerializationServiceV1(serialization_config=config)
+
+        d1 = service.to_data(byte_val)
+        d2 = service.to_data(short_val)
+        d3 = service.to_data(int_val)
+
+        self.assertEqual(d1.get_type(), CONSTANT_TYPE_INTEGER)
+        self.assertEqual(d2.get_type(), CONSTANT_TYPE_INTEGER)
+        self.assertEqual(d3.get_type(), CONSTANT_TYPE_INTEGER)
+
+    def test_long_case(self):
+        config = SerializationConfig()
+        config.default_integer_type = INTEGER_TYPE.LONG
+        service = SerializationServiceV1(serialization_config=config)
+
+        d1 = service.to_data(byte_val)
+        d2 = service.to_data(short_val)
+        d3 = service.to_data(int_val)
+        d4 = service.to_data(long_val)
+
+        self.assertEqual(d1.get_type(), CONSTANT_TYPE_LONG)
+        self.assertEqual(d2.get_type(), CONSTANT_TYPE_LONG)
+        self.assertEqual(d3.get_type(), CONSTANT_TYPE_LONG)
+        self.assertEqual(d4.get_type(), CONSTANT_TYPE_LONG)
