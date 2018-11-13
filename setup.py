bd4195fa0d3a1a5d4cf88d0d8fca267613c8747f
Metin Dumandağ <metindumandag@gmail.com>
2018-11-13 12:17:20 +0300

    Readme update (#129)
    * Update README and setup.py
    
    Update the README with new document structure and contents.
    Also, update the setup.py to correctly identify the new markdown
    style.
    
    * simplify code examples' README
    
    * fix typos, refactor query section and fix sample codes
    
    * change docs structure to api doc only
    
    * fix typos

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from hazelcast import __version__
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -13,6 +13,7 @@ setup(
         version=__version__,
         description='Hazelcast Python Client',
         long_description=long_description,
+        long_description_content_type='text/markdown',
         url='https://github.com/hazelcast/hazelcast-python-client',
 
         author='Hazelcast Inc. Developers',
