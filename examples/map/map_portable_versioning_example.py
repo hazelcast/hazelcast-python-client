c3e15a141ef4a86e21eda7db71836764fd82e5f9
yagmurdulger <50118327+yagmurdulger@users.noreply.github.com>
2019-07-08 14:42:01 +0300

    Portable Serialization Multiversion Support Code Sample (#175)
    * Sample code for portable versioning
    
    * Portable Versioning Example. Added comments and explanations.
    
    * Portable versioning example. Fixed with requested changes.

---
+++ examples/map/map_portable_versioning_example.py
@@ -0,0 +1,184 @@
+import hazelcast
+
+from hazelcast.serialization.api import Portable
+
+# This sample code demonstrates multiversion support of Portable serialization.
+
+# With multiversion support, you can have two members that have different
+# versions of the same object, and Hazelcast will store both meta information and use the
+# correct one to serialize and deserialize portable objects depending on the member.
+
+# Default (version 1) Employee class.
+class Employee(Portable):
+    FACTORY_ID = 666
+    CLASS_ID = 1
+
+    def __init__(self, name=None, age=None):
+        self.name = name
+        self.age = age
+
+    def write_portable(self, writer):
+        writer.write_utf("name", self.name)
+        writer.write_int("age", self.age)
+
+    def read_portable(self, reader):
+        self.name = reader.read_utf("name")
+        self.age = reader.read_int("age")
+
+    def get_factory_id(self):
+        return self.FACTORY_ID
+
+    def get_class_id(self):
+        return self.CLASS_ID
+
+    def __str__(self):
+        return "Employee[ name:{} age:{} ]".format(self.name, self.age)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.name == other.name and self.age == other.age
+
+# If you update the class by changing the type of one of the fields or by adding a new field,
+# it is a good idea to upgrade the version of the class, rather than sticking to the global versioning
+# that is specified in the hazelcast.xml file.
+
+# Version 2: Added new field manager name (str).
+class Employee2(Portable):
+    FACTORY_ID = 666
+    CLASS_ID = 1
+    CLASS_VERSION = 2 # specifies version different than the global version
+
+    def __init__(self, name=None, age=None, manager=None):
+        self.name = name
+        self.age = age
+        self.manager = manager
+
+    def write_portable(self, writer):
+        writer.write_utf("name", self.name)
+        writer.write_int("age", self.age)
+        writer.write_utf("manager", self.manager)
+
+    def read_portable(self, reader):
+        self.name = reader.read_utf("name")
+        self.age = reader.read_int("age")
+        self.manager = reader.read_utf("manager")
+
+    def get_factory_id(self):
+        return self.FACTORY_ID
+
+    def get_class_id(self):
+        return self.CLASS_ID
+
+    # It is necessary to implement this method for multiversion support to work
+    def get_class_version(self):
+        return self.CLASS_VERSION
+
+    def __str__(self):
+        return "Employee[ name:{} age:{} manager:{} ]".format(self.name, self.age, self.manager)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.name == other.name and self.age == other.age \
+               and self.manager == other.manager
+
+
+# However, having a version that changes across incompatible field types such as int and String will cause
+# a type error as members with older versions of the class tries to access it. We will demonstrate this below.
+
+# Version3: Changed age field type from int to String. (Incompatible type change)
+class Employee3(Portable):
+    FACTORY_ID = 666
+    CLASS_ID = 1
+    CLASS_VERSION = 3
+
+    def __init__(self, name=None, age=None, manager=None):
+        self.name = name
+        self.age = age
+        self.manager = manager
+
+    def write_portable(self, writer):
+        writer.write_utf("name", self.name)
+        writer.write_utf("age", self.age)
+        writer.write_utf("manager", self.manager)
+
+    def read_portable(self, reader):
+        self.name = reader.read_utf("name")
+        self.age = reader.read_utf("age")
+        self.manager = reader.read_utf("manager")
+
+    def get_factory_id(self):
+        return self.FACTORY_ID
+
+    def get_class_id(self):
+        return self.CLASS_ID
+
+    def get_class_version(self):
+        return self.CLASS_VERSION
+
+    def __str__(self):
+        return "Employee[ name:{} age:{} manager:{} ]".format(self.name, self.age, self.manager)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.name == other.name and self.age == other.age \
+               and self.manager == other.manager
+
+
+if __name__ == '__main__':
+
+    # Let's now configure 3 clients with 3 different versions of Employee.
+    config = hazelcast.ClientConfig()
+    config.serialization_config.portable_factories[Employee.FACTORY_ID] = \
+        {Employee.CLASS_ID: Employee}
+    client = hazelcast.HazelcastClient(config)
+
+    config2 = hazelcast.ClientConfig()
+    config2.serialization_config.portable_factories[Employee2.FACTORY_ID] = \
+        {Employee2.CLASS_ID: Employee2}
+    client2 = hazelcast.HazelcastClient(config2)
+
+    config3 = hazelcast.ClientConfig()
+    config3.serialization_config.portable_factories[Employee3.FACTORY_ID] = \
+        {Employee3.CLASS_ID: Employee3}
+    client3 = hazelcast.HazelcastClient(config3)
+
+    # Assume that a member joins a cluster with a newer version of a class.
+    # If you modified the class by adding a new field, the new member's put operations include that
+    # new field.
+    my_map = client.get_map("employee-map").blocking()
+    my_map2 = client2.get_map("employee-map").blocking()
+
+    my_map.clear()
+    my_map.put(0, Employee("Jack", 28))
+    my_map2.put(1, Employee2("Jane", 29, "Josh"))
+
+    print('Map Size: {}'.format(my_map.size()))
+
+    # If this new member tries to get an object that was put from the older members, it
+    # gets null for the newly added field.
+    for v in my_map.values():
+        print(v)
+
+    for v in my_map2.values():
+        print(v)
+
+    # Let's try now to put a version 3 Employee object to the map and see what happens.
+    my_map3 = client3.get_map("employee-map").blocking()
+    my_map3.put(2, Employee3("Joe", "30", "Mary"))
+
+    print('Map Size: {}'.format(my_map.size()))
+
+    # As clients with incompatible versions of the class try to access each other, a HazelcastSerializationError
+    # is raised (caused by a TypeError).
+    try:
+        # Client that has class with int type age field tries to read Employee3 object with String age field.
+        print(my_map.get(2))
+    except hazelcast.exception.HazelcastSerializationError as ex:
+        print("Failed due to: {}".format(ex))
+
+    try:
+        # Client that has class with String type age field tries to read Employee object with int age field.
+        print(my_map3.get(0))
+    except hazelcast.exception.HazelcastSerializationError as ex:
+        print("Failed due to: {}".format(ex))
+
+    client.shutdown()
+    client2.shutdown()
+    client3.shutdown()
