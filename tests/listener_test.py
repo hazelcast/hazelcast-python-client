d332ffb083ec984cac290768de512d3a20c88737
Yagmur Dulger <yd333@cornell.edu>
2019-07-30 17:48:55 +0300

    Fixes for connection related listener tests.

--- tests/listener_test.py
+++ tests/listener_test.py
@@ -4,16 +4,6 @@ from hazelcast.config import ClientConfig
 
 
 class ListenerTest(HazelcastTestCase):
-    # @classmethod
-    # def setUpClass(cls):
-    #     configure_logging()
-    #     cls.rc = cls.create_rc()
-    #     cls.cluster = cls.create_cluster(cls.rc, None)
-    #
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.rc.exit()
-
     def setUp(self):
         configure_logging()
         self.rc = self.create_rc()
@@ -43,35 +33,26 @@ class ListenerTest(HazelcastTestCase):
             self.assertEqual(1, len(self.collector.events))
         self.assertTrueEventually(assert_event)
 
-    def test_non_smart_listener_remove_partition_owner_member(self):
+    def test_non_smart_listener_remove_connected_member(self):
         self.client_config.network_config.smart_routing = False
         client = self.create_client(self.client_config)
         map = client.get_map(random_string()).blocking()
-        key_m1 = generate_key_owned_by_instance(client, self.m1.address)
-        map.put(key_m1, 'value1')
-        map.add_entry_listener(updated_func=self.collector)
-        self.m1.shutdown()
-        map.put(key_m1, 'value2')
-        event_count = len(self.collector.events)
-
-        def assert_event():
-            self.assertEqual(1, event_count)
-        self.assertTrueEventually(assert_event)
-
-    def test_non_smart_listener_remove_connection_owner_member(self):
-        self.client_config.network_config.smart_routing = False
-        client = self.create_client(self.client_config)
-        map = client.get_map(random_string()).blocking()
-        map.add_entry_listener(added_func=self.collector, updated_func=self.collector)
+        map.add_entry_listener(added_func=self.collector)
 
         owner_address = client.cluster.owner_connection_address
-        generated_key = generate_key_owned_by_instance(client, owner_address)  # it might not be necessary to generate
-        # key for this test. should i remove it to simplify the code?
-        map.put(generated_key, 'value1')
-        for m in [self.m1, self.m2, self.m3]:
+
+        # Test listener re-registers properly when owner connection is removed.
+        members = [self.m1, self.m2, self.m3]
+        for m in members:
             if m.address == owner_address:
                 m.shutdown()
-        map.put(generated_key, 'value2')
+                members.remove(m)
+
+        # There are 2 members left. We execute a put operation to each of their partitions
+        # to test that non-smart listener works in both local and non-local cases.
+        for m in members:
+            generated_key = generate_key_owned_by_instance(client, m.address)
+            map.put(generated_key, 'value')
 
         def assert_event():
             self.assertEqual(2, len(self.collector.events))
@@ -103,14 +84,3 @@ class ListenerTest(HazelcastTestCase):
         def assert_event():
             self.assertEqual(1, len(self.collector.events))
         self.assertTrueEventually(assert_event)
-
-    # ------------------------------------------------------------------ #
-    # Helpers:
-    # def remove_member_test(self, client):
-    #     map = client.get_map(random_string()).blocking()
-    #     key_m1 = generate_key_owned_by_instance(client, self.m1.address)
-    #     map.put(key_m1, 'value1')
-    #     map.add_entry_listener(updated_func=self.collector)
-    #     self.m1.shutdown()
-    #     map.put(key_m1, 'value2')
-    #     return len(self.collector.events)
