e259d869c88f9bdf9c88d6f8fd32063496c37776
Metin Dumandag <metin@hazelcast.com>
2021-03-24 17:47:18 +0300

    Fail fast on the METADATA group name for CP data structures (#380)
    It is not possible to run the CP data structures on the metadata
    group name. We now fail-fast if someone tries to use the `METADATA`
    as the CP group name. Also, the group name comparison made
    case-insensitive.

--- tests/unit/proxy/cp/cp_proxy_manager_test.py
+++ tests/unit/proxy/cp/cp_proxy_manager_test.py
@@ -6,11 +6,19 @@ from hazelcast.cp import _without_default_group_name, _get_object_name_for_proxy
 class CPProxyManagerTest(unittest.TestCase):
     def test_without_default_group_name(self):
         self.assertEqual("test", _without_default_group_name("test@default"))
+        self.assertEqual("test", _without_default_group_name("test@DEFAULT"))
         self.assertEqual("test@custom", _without_default_group_name("test@custom"))
 
     def test_without_default_group_name_with_multiple_group_names(self):
         with self.assertRaises(AssertionError):
             _without_default_group_name("test@default@@default")
+
+    def test_without_default_group_name_with_metadata_group_name(self):
+        with self.assertRaises(AssertionError):
+            _without_default_group_name("test@METADATA")
+
+        with self.assertRaises(AssertionError):
+            _without_default_group_name("test@metadata")
 
     def test_get_object_name_for_proxy(self):
         self.assertEqual("test", _get_object_name_for_proxy("test@default"))
