faa52075094954ba36b98660413d6581de1e03e6
mdumandag <metin@hazelcast.com>
2021-04-27 10:06:06 +0300

    do not download dependencies transitively

--- start_rc.py
+++ start_rc.py
@@ -47,6 +47,7 @@ def download_if_necessary(repo, artifact_id, version, is_test_artifact=False):
         "mvn",
         "-q",
         "dependency:get",
+        "-Dtransitive=false",
         "-DrepoUrl=" + repo,
         "-Dartifact=" + artifact,
         "-Ddest=" + dest_file_name,
