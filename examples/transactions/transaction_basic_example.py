f8c2b9bbf50621934b024d00c6dde877115aca7a
Metin Dumandag <metin@hazelcast.com>
2021-06-10 14:49:14 +0300

    Show the context manager support for transactions in code samples and documentation (#410)
    We do have a context manager support for transactions but we were
    not showing it in our code samples or documentation. As a result
    of that, users were unaware of this feature. We even had a call
    with one user who implemented a simple context manager for
    Hazelcast transactions himself.
    
    So, with this PR, we are now showing that we have support for it
    in our code samples and documentation.

--- examples/transactions/transaction_basic_example.py
+++ examples/transactions/transaction_basic_example.py
@@ -2,17 +2,11 @@ import hazelcast
 import time
 
 client = hazelcast.HazelcastClient()
-transaction = client.new_transaction(timeout=10)
-try:
-    transaction.begin()
-    transactional_map = transaction.get_map("transaction-map")
-    print("Map: {}".format(transactional_map))
+
+with client.new_transaction(timeout=10) as transaction:
+    transactional_map = transaction.get_map("transactional-map")
+    print("Created map:", transactional_map)
 
     transactional_map.put("1", "1")
     time.sleep(0.1)
     transactional_map.put("2", "2")
-
-    transaction.commit()
-except Exception as ex:
-    transaction.rollback()
-    print("Transaction failed! {}".format(ex.args))
