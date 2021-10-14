f1db480a5d2d2103ee6577d066586eeddd74865a
Metin Dumandag <metin@hazelcast.com>
2021-10-14 13:03:10 +0300

    [BACKPORT] Calculate the initial frame size of the client messages correctly (#499)
    * Calculate the initial frame size of the client messages correctly
    
    While calculating the initial frame size of the client messages,
    we were adding `SIZE_OF_FRAME_LENGTH_AND_FLAGS` twice. That extra
    addition is removed from the `create_initial_buffer`.
    
    To make the `create_initial_buffer_custom` alike, I also removed
    the addition from there, although there was no such problem for that
    method. To make it work again, I have modified the protocol template
    to perform one less subtraction of `SIZE_OF_FRAME_LENGTH_AND_FLAGS`
    from the initial frame size.
    
    Also, while I was there, I have updated the `sql_execute_codec`, and
    added support for the newly added parameter.
    
    * add a unit test that verifies the encoded requests produce the same bytearray as the Java client
    
    * bump client version
    
    * add v5 to branch filter in test runner

--- hazelcast/protocol/codec/sql_execute_codec.py
+++ hazelcast/protocol/codec/sql_execute_codec.py
@@ -18,15 +18,17 @@ _RESPONSE_MESSAGE_TYPE = 2163713
 _REQUEST_TIMEOUT_MILLIS_OFFSET = REQUEST_HEADER_SIZE
 _REQUEST_CURSOR_BUFFER_SIZE_OFFSET = _REQUEST_TIMEOUT_MILLIS_OFFSET + LONG_SIZE_IN_BYTES
 _REQUEST_EXPECTED_RESULT_TYPE_OFFSET = _REQUEST_CURSOR_BUFFER_SIZE_OFFSET + INT_SIZE_IN_BYTES
-_REQUEST_INITIAL_FRAME_SIZE = _REQUEST_EXPECTED_RESULT_TYPE_OFFSET + BYTE_SIZE_IN_BYTES
+_REQUEST_SKIP_UPDATE_STATISTICS_OFFSET = _REQUEST_EXPECTED_RESULT_TYPE_OFFSET + BYTE_SIZE_IN_BYTES
+_REQUEST_INITIAL_FRAME_SIZE = _REQUEST_SKIP_UPDATE_STATISTICS_OFFSET + BOOLEAN_SIZE_IN_BYTES
 _RESPONSE_UPDATE_COUNT_OFFSET = RESPONSE_HEADER_SIZE
 
 
-def encode_request(sql, parameters, timeout_millis, cursor_buffer_size, schema, expected_result_type, query_id):
+def encode_request(sql, parameters, timeout_millis, cursor_buffer_size, schema, expected_result_type, query_id, skip_update_statistics):
     buf = create_initial_buffer(_REQUEST_INITIAL_FRAME_SIZE, _REQUEST_MESSAGE_TYPE)
     FixSizedTypesCodec.encode_long(buf, _REQUEST_TIMEOUT_MILLIS_OFFSET, timeout_millis)
     FixSizedTypesCodec.encode_int(buf, _REQUEST_CURSOR_BUFFER_SIZE_OFFSET, cursor_buffer_size)
     FixSizedTypesCodec.encode_byte(buf, _REQUEST_EXPECTED_RESULT_TYPE_OFFSET, expected_result_type)
+    FixSizedTypesCodec.encode_boolean(buf, _REQUEST_SKIP_UPDATE_STATISTICS_OFFSET, skip_update_statistics)
     StringCodec.encode(buf, sql)
     ListMultiFrameCodec.encode_contains_nullable(buf, parameters, DataCodec.encode)
     CodecUtil.encode_nullable(buf, schema, StringCodec.encode)
