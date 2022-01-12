ff67e0aeec75466a4301711e7170de30f8649f84
mdumandag <metin@hazelcast.com>
2022-01-12 10:57:02 +0300

    Make the tests more resilient to timing problems
    We had some test failures on the backward compatibility runs, mainly
    due to the fact that, the failing tests were not resilient enough
    to timing problems.
    
    In the `HeartbeatTest::test_heartbeat_stopped_and_restored`, it was
    possible that the responses coming from the server for the previous
    heartbeat requests to override what we wrote as the `last_read_time`.
    
    In the `ListenerAddMemberTest::test_add_member`, it was possible that
    the `map#put` would complete before the listener is registered to the
    newly added connection.

--- tests/integration/backward_compatible/heartbeat_test.py
+++ tests/integration/backward_compatible/heartbeat_test.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 from hazelcast import HazelcastClient
 from hazelcast.core import Address
 from tests.base import HazelcastTestCase
@@ -5,6 +8,8 @@ from tests.util import open_connection_to_address, wait_for_partition_table
 
 
 class HeartbeatTest(HazelcastTestCase):
+    rc = None
+
     @classmethod
     def setUpClass(cls):
         cls.rc = cls.create_rc()
@@ -48,25 +53,57 @@ class HeartbeatTest(HazelcastTestCase):
             connection_added_collector, connection_removed_collector
         )
 
-        self.simulate_heartbeat_lost(self.client, addr, 2)
+        start_simulation = threading.Event()
+        assertion_succeeded = [False]
+        simulation_thread = self.simulate_heartbeat_loss(
+            self.client, addr, 2, start_simulation, assertion_succeeded
+        )
 
         def assert_heartbeat_stopped_and_restored():
-            self.assertEqual(1, len(connection_added_collector.connections))
-            self.assertEqual(1, len(connection_removed_collector.connections))
-            stopped_connection = connection_added_collector.connections[0]
-            restored_connection = connection_removed_collector.connections[0]
-            self.assertEqual(
-                stopped_connection.connected_address, Address(member2.host, member2.port)
-            )
-            self.assertEqual(
-                restored_connection.connected_address, Address(member2.host, member2.port)
-            )
+            self.assertTrue(len(connection_added_collector.connections) >= 1)
+            self.assertTrue(len(connection_removed_collector.connections) >= 1)
 
+            stopped_connection = connection_removed_collector.connections[0]
+            restored_connection = connection_added_collector.connections[0]
+
+            self.assertEqual(
+                stopped_connection.connected_address,
+                Address(member2.host, member2.port),
+            )
+            self.assertEqual(
+                restored_connection.connected_address,
+                Address(member2.host, member2.port),
+            )
+            assertion_succeeded[0] = True
+
+        start_simulation.set()
         self.assertTrueEventually(assert_heartbeat_stopped_and_restored)
+        simulation_thread.join()
 
     @staticmethod
-    def simulate_heartbeat_lost(client, address, timeout):
-        for connection in client._connection_manager.active_connections.values():
-            if connection.remote_address == address:
-                connection.last_read_time -= timeout
-                break
+    def simulate_heartbeat_loss(client, address, timeout, start_simulation, assertion_succeeded):
+        def run():
+            # Wait until the main thread signals we should start the simulation
+            start_simulation.wait()
+
+            # It is possible for client to override the set last_read_time
+            # of the connection, in case of the periodically sent heartbeat
+            # requests getting responses, right after we try to set a new
+            # value to it, before the next iteration of the heartbeat manager.
+            # In this case, the connection won't be closed, and the test would
+            # fail. To avoid it, we will try multiple times.
+            for i in range(10):
+                if assertion_succeeded[0]:
+                    # We have successfully simulated heartbeat loss
+                    return
+
+                for connection in client._connection_manager.active_connections.values():
+                    if connection.remote_address == address:
+                        connection.last_read_time -= timeout
+                        break
+
+                time.sleep((i + 1) * 0.1)
+
+        t = threading.Thread(target=run)
+        t.start()
+        return t
