012ea441aaf5a2be51a192be3b4153194f4ba64c
mdumandag <metin@hazelcast.com>
2022-02-18 11:06:50 +0300

    Update copyright year to 2022

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"Hazelcast Python Client"
-copyright = u"2021, Hazelcast Inc."
+copyright = u"2022, Hazelcast Inc."
 author = u"Hazelcast Inc. Developers"
 
 # The version info for the project you're documenting, acts as replacement for
--- docs/copyright.rst
+++ docs/copyright.rst
@@ -1,7 +1,7 @@
 Copyright
 =========
 
-Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
 
 Visit `hazelcast.com <https://hazelcast.com>`__ for more
 information.
