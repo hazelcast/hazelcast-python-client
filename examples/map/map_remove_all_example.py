3feb0c272a720d89fe1ceb9098bfa620a1a6cea5
Feyza <60661718+feysahin@users.noreply.github.com>
2022-09-05 10:24:31 +0300

    [API-1499] Near-cache invalidation is added for remove_all and minor styling issues. (#568)
    remove_all implementation added

--- examples/map/map_remove_all_example.py
+++ examples/map/map_remove_all_example.py
@@ -5,9 +5,8 @@ from hazelcast.predicate import between
 client = hazelcast.HazelcastClient()
 
 predicate_map = client.get_map("predicate-map").blocking()
-
 for i in range(10):
-    predicate_map.put("key" + str(i), i)
+    predicate_map.put(f"key{i}", i)
 
 predicate = between("this", 3, 5)
 
