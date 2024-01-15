43810dd98e73af650bffd40e2be6bc6390c6a753
Jack Green <jack.green@hazelcast.com>
2024-01-15 17:11:54 +0000

    Revert vscode reformat

--- docs/conf.py
+++ docs/conf.py
@@ -276,13 +276,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (
-        master_doc,
-        "hazelcast-python-client",
-        "Hazelcast Python Client Documentation",
-        [author],
-        1,
-    )
+    (master_doc, "hazelcast-python-client", "Hazelcast Python Client Documentation", [author], 1)
 ]
 
 # If true, show URL addresses after external links.
