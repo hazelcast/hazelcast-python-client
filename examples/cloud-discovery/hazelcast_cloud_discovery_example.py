30c6b419da86cac240e75683d306654c4713a1c2
Yuce Tekol <yuce.tekol@hazelcast.com>
2024-05-17 12:21:17 +0300

    restore cloud discovery example

--- examples/cloud-discovery/hazelcast_cloud_discovery_example.py
+++ examples/cloud-discovery/hazelcast_cloud_discovery_example.py
@@ -2,16 +2,16 @@ import hazelcast
 
 client = hazelcast.HazelcastClient(
     # Set up cluster name for authentication
-    cluster_name="vprod",
+    cluster_name="YOUR_CLUSTER_NAME",
     # Set the token of your cloud cluster
-    cloud_discovery_token="c1rZO25ftRaQknBqMgWF3yhdsvNriFMgsjMrFPNWYPNdvx3U2Z",
+    cloud_discovery_token="YOUR_CLUSTER_DISCOVERY_TOKEN",
     # If you have enabled encryption for your cluster, also configure TLS/SSL for the client.
     # Otherwise, skip options below.
     ssl_enabled=True,
-    ssl_cafile="/dev/shm/ssl/ca.pem",
-    ssl_certfile="/dev/shm/ssl/cert.pem",
-    ssl_keyfile="/dev/shm/ssl/key.pem",
-    ssl_password="1c2670d289d",
+    ssl_cafile="/path/to/ca.pem",
+    ssl_certfile="/path/to/cert.pem",
+    ssl_keyfile="/path/to/key.pem",
+    ssl_password="YOUR_KEY_STORE_PASSWORD",
 )
 
 my_map = client.get_map("map-on-the-cloud").blocking()
