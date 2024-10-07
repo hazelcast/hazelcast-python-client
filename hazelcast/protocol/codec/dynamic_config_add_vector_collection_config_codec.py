b43aad14f610385659fe7ff2224ab58d3a1c32f8
Krzysztof Jamróz <79092062+k-jamroz@users.noreply.github.com>
2024-10-07 10:28:05 +0200

    Add support for backup count in Vector Collection configuration [AI-145] (#702)
    Backup count configuration for vector collection was added in
    https://github.com/hazelcast/hazelcast-client-protocol/pull/533
    
    New parameters need #699 but the tests work with both 5.5 (skipped) and
    6.0.
    
    Co-authored-by: Yüce Tekol <yuce.tekol@hazelcast.com>

--- hazelcast/protocol/codec/dynamic_config_add_vector_collection_config_codec.py
+++ hazelcast/protocol/codec/dynamic_config_add_vector_collection_config_codec.py
@@ -1,3 +1,5 @@
+from hazelcast.serialization.bits import *
+from hazelcast.protocol.builtin import FixSizedTypesCodec
 from hazelcast.protocol.client_message import OutboundMessage, REQUEST_HEADER_SIZE, create_initial_buffer
 from hazelcast.protocol.builtin import StringCodec
 from hazelcast.protocol.builtin import ListMultiFrameCodec
@@ -8,11 +10,15 @@ _REQUEST_MESSAGE_TYPE = 1774592
 # hex: 0x1B1401
 _RESPONSE_MESSAGE_TYPE = 1774593
 
-_REQUEST_INITIAL_FRAME_SIZE = REQUEST_HEADER_SIZE
+_REQUEST_BACKUP_COUNT_OFFSET = REQUEST_HEADER_SIZE
+_REQUEST_ASYNC_BACKUP_COUNT_OFFSET = _REQUEST_BACKUP_COUNT_OFFSET + INT_SIZE_IN_BYTES
+_REQUEST_INITIAL_FRAME_SIZE = _REQUEST_ASYNC_BACKUP_COUNT_OFFSET + INT_SIZE_IN_BYTES
 
 
-def encode_request(name, index_configs):
+def encode_request(name, index_configs, backup_count, async_backup_count):
     buf = create_initial_buffer(_REQUEST_INITIAL_FRAME_SIZE, _REQUEST_MESSAGE_TYPE)
+    FixSizedTypesCodec.encode_int(buf, _REQUEST_BACKUP_COUNT_OFFSET, backup_count)
+    FixSizedTypesCodec.encode_int(buf, _REQUEST_ASYNC_BACKUP_COUNT_OFFSET, async_backup_count)
     StringCodec.encode(buf, name)
     ListMultiFrameCodec.encode(buf, index_configs, VectorIndexConfigCodec.encode, True)
     return OutboundMessage(buf, False)
