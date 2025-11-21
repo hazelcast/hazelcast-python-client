62697e31e9f696841c23a412e6174a6d40a5b05a
Yuce Tekol <yucetekol@gmail.com>
2025-11-21 17:10:29 +0300

    Add BETA warning

--- hazelcast/asyncio/__init__.py
+++ hazelcast/asyncio/__init__.py
@@ -1,2 +1,10 @@
+import warnings
+
+warnings.warn("Asyncio API for Hazelcast Python Client is in BETA. DO NOT use it in production.")
+del warnings
+
+__all__ = ["HazelcastClient", "Map"]
+
 from hazelcast.asyncio.client import HazelcastClient
 from hazelcast.internal.asyncio_proxy.map import Map
+
