f49d8ab6075d8ac00db30fbcc790809514cfc259
Yüce Tekol <yuce.tekol@hazelcast.com>
2025-11-28 12:14:40 +0300

    [API-2326] Initial Asyncio Module PR [1/6] (#741)
    This is the initial asyncio support.
    
    * Added the `asyncio` module which contains public asyncio API
    * Added the `internal` module and `internal/asyncio_` modules, which
    contains the private asyncio API/implementation.
    * Added asyncio support for the Map proxy. Currently near cache,
    transactions, and lock releated-methods are not supported. The near
    cache support will come in another PR. Transactions will likely not be
    supported. Locks may need a different design.
    * I didn't include the API docs, in order to make the PR smaller. I'll
    add them in another PR.
    * The following tests are ported from the old API, and they work without
    changes (besides making them compatible with async/await syntax):
      - `tests/integration/asyncio/authentication_tests`
      -  `tests/integration/asyncio/backup_acks_tests`
    - `tests/integration/asyncio/client_test` (*one test is not ported, due
    to its Topic DDS dependency*)
      -  `tests/integration/asyncio/proxy/map_test`
    
    Most of the code in this PR was duplicated to the `internal` module by
    prefixing them with `asyncio_`. For example `ROOT/cluster.py` was
    duplicated/modifed as `internal/asyncio_cluster.py`
    
    Here is the diff between modules in this PR vs their counterparts in the
    old API:
    https://gist.github.com/yuce/56e79a29a1d4d1d996788381d489c0a4

---
+++ tests/integration/asyncio/authentication_tests/authentication_test.py
@@ -0,0 +1,70 @@
+import os
+import unittest
+
+import pytest
+
+from hazelcast.errors import HazelcastError
+from tests.integration.asyncio.base import HazelcastTestCase
+from tests.util import get_abs_path, compare_client_version
+from hazelcast.asyncio.client import HazelcastClient
+
+try:
+    from hazelcast.security import BasicTokenProvider
+except ImportError:
+    pass
+
+
+@pytest.mark.enterprise
+@unittest.skipIf(
+    compare_client_version("4.2.1") < 0, "Tests the features added in 4.2.1 version of the client"
+)
+class AuthenticationTest(unittest.IsolatedAsyncioTestCase, HazelcastTestCase):
+    current_directory = os.path.dirname(__file__)
+    rc = None
+    hazelcast_token_xml = get_abs_path(
+        current_directory, "../../backward_compatible/authentication_tests/hazelcast-token.xml"
+    )
+    hazelcast_userpass_xml = get_abs_path(
+        current_directory, "../../backward_compatible/authentication_tests/hazelcast-user-pass.xml"
+    )
+
+    def setUp(self):
+        self.rc = self.create_rc()
+
+    def tearDown(self):
+        self.rc.exit()
+
+    async def test_no_auth(self):
+        cluster = self.create_cluster(self.rc, self.configure_cluster(self.hazelcast_userpass_xml))
+        cluster.start_member()
+
+        with self.assertRaises(HazelcastError):
+            await HazelcastClient.create_and_start(
+                cluster_name=cluster.id, cluster_connect_timeout=2
+            )
+
+    async def test_token_auth(self):
+        cluster = self.create_cluster(self.rc, self.configure_cluster(self.hazelcast_token_xml))
+        cluster.start_member()
+
+        token_provider = BasicTokenProvider("Hazelcast")
+        client = await HazelcastClient.create_and_start(
+            cluster_name=cluster.id, token_provider=token_provider
+        )
+        self.assertTrue(client.lifecycle_service.is_running())
+        await client.shutdown()
+
+    async def test_username_password_auth(self):
+        cluster = self.create_cluster(self.rc, self.configure_cluster(self.hazelcast_userpass_xml))
+        cluster.start_member()
+
+        client = await HazelcastClient.create_and_start(
+            cluster_name=cluster.id, creds_username="member1", creds_password="s3crEt"
+        )
+        self.assertTrue(client.lifecycle_service.is_running())
+        await client.shutdown()
+
+    @classmethod
+    def configure_cluster(cls, filename):
+        with open(filename, "r") as f:
+            return f.read()
---
+++ tests/integration/asyncio/client_test.py
@@ -0,0 +1,141 @@
+import unittest
+
+from tests.integration.asyncio.base import HazelcastTestCase, SingleMemberTestCase
+from hazelcast.asyncio.client import HazelcastClient
+from tests.hzrc.ttypes import Lang
+from tests.util import compare_client_version, random_string
+
+try:
+    from hazelcast.config import Config
+    from hazelcast.errors import InvalidConfigurationError
+except ImportError:
+    # For backward compatibility tests
+    pass
+
+
+class ClientLabelsTest(unittest.IsolatedAsyncioTestCase, HazelcastTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.rc = cls.create_rc()
+        cls.cluster = cls.create_cluster(cls.rc)
+        cls.cluster.start_member()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.rc.terminateCluster(cls.cluster.id)
+        cls.rc.exit()
+
+    async def asyncTearDown(self):
+        await self.shutdown_all_clients()
+
+    async def test_default_config(self):
+        client = await self.create_client({"cluster_name": self.cluster.id})
+        self.assertIsNone(self.get_labels_from_member(client._connection_manager.client_uuid))
+
+    async def test_provided_labels_are_received(self):
+        client = await self.create_client(
+            {
+                "cluster_name": self.cluster.id,
+                "labels": [
+                    "test-label",
+                ],
+            }
+        )
+        self.assertEqual(
+            b"test-label", self.get_labels_from_member(client._connection_manager.client_uuid)
+        )
+
+    def get_labels_from_member(self, client_uuid):
+        script = """
+        var clients = instance_0.getClientService().getConnectedClients().toArray();
+        for (i=0; i < clients.length; i++) {
+            var client = clients[i];
+            if ("%s".equals(client.getUuid().toString())) {
+                result = client.getLabels().iterator().next();
+                break;
+            }
+        }""" % str(
+            client_uuid
+        )
+        return self.rc.executeOnController(self.cluster.id, script, Lang.JAVASCRIPT).result
+
+
+@unittest.skipIf(
+    compare_client_version("4.2.2") < 0 or compare_client_version("5.0") == 0,
+    "Tests the features added in 5.1 version of the client, "
+    "which are backported into 4.2.2 and 5.0.1",
+)
+class ClientTcpMetricsTest(SingleMemberTestCase):
+    @classmethod
+    def configure_client(cls, config):
+        config["cluster_name"] = cls.cluster.id
+        return config
+
+    async def test_bytes_received(self):
+        reactor = self.client._reactor
+        bytes_received = reactor._bytes_received
+        self.assertGreater(bytes_received, 0)
+        m = await self.client.get_map(random_string())
+        await m.get(random_string())
+        self.assertGreater(reactor._bytes_received, bytes_received)
+
+    async def test_bytes_sent(self):
+        reactor = self.client._reactor
+        bytes_sent = reactor._bytes_sent
+        self.assertGreater(bytes_sent, 0)
+        m = await self.client.get_map(random_string())
+        await m.set(random_string(), random_string())
+        self.assertGreater(reactor._bytes_sent, bytes_sent)
+
+
+@unittest.skipIf(
+    compare_client_version("5.2") < 0,
+    "Tests the features added in 5.2 version of the client",
+)
+class ClientConfigurationTest(unittest.IsolatedAsyncioTestCase, HazelcastTestCase):
+    rc = None
+    cluster = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.rc = cls.create_rc()
+        cls.cluster = cls.create_cluster(cls.rc, None)
+        cls.cluster.start_member()
+
+    def setUp(self):
+        self.client = None
+
+    async def asyncTearDown(self):
+        if self.client:
+            await self.client.shutdown()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.rc.terminateCluster(cls.cluster.id)
+        cls.rc.exit()
+
+    async def test_keyword_args_configuration(self):
+        self.client = await HazelcastClient.create_and_start(
+            cluster_name=self.cluster.id,
+        )
+        self.assertTrue(self.client.lifecycle_service.is_running())
+
+    async def test_configuration_object(self):
+        config = Config()
+        config.cluster_name = self.cluster.id
+        self.client = await HazelcastClient.create_and_start(config)
+        self.assertTrue(self.client.lifecycle_service.is_running())
+
+    async def test_configuration_object_as_keyword_argument(self):
+        config = Config()
+        config.cluster_name = self.cluster.id
+        self.client = await HazelcastClient.create_and_start(config=config)
+        self.assertTrue(self.client.lifecycle_service.is_running())
+
+    async def test_ambiguous_configuration(self):
+        config = Config()
+        with self.assertRaisesRegex(
+            InvalidConfigurationError,
+            "Ambiguous client configuration is found",
+        ):
+            self.client = await HazelcastClient.create_and_start(config, cluster_name="a-cluster")
