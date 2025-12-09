15772fe19c2d2d2ed656da72edd123ab4263542a
Yuce Tekol <yucetekol@gmail.com>
2025-12-09 07:52:48 +0300

    Remove Python 3.7

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
