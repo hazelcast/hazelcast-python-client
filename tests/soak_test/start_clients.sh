6cb5b172bf3d2a00c27272e8b55e54cf41638be9
Yuce Tekol <yucetekol@gmail.com>
2025-12-09 06:45:26 +0300

    Stop clients on exit

--- tests/soak_test/start_clients.sh
+++ tests/soak_test/start_clients.sh
@@ -5,17 +5,18 @@ DURATION=${2:-48.0}
 
 mkdir -p "client_logs"
 
-#for i in {1..5}
-#do
-#    python map_soak_test.py \
-#        --duration "$DURATION" \
-#        --address "$ADDRESS" \
-#        --log-file client_logs/client-"$i" &
-#done
-
 declare -a pids
 
-for i in {1..2}; do
+cleanup () {
+  for pid in "${pids[@]}"; do
+    echo "Stopping $pid"
+    kill "$pid"
+  done
+}
+
+trap cleanup EXIT
+
+for i in {1..5}; do
   python map_soak_test_asyncio.py \
       --duration "$DURATION" \
       --address "$ADDRESS" \
@@ -25,7 +26,14 @@ for i in {1..2}; do
   pids+=("$pid")
 done
 
-for pid in "${pids[@]}"; do
-  echo "Waiting for $pid to end"
-  wait $pid
+for i in {1..5}; do
+  python map_soak_test.py \
+      --duration "$DURATION" \
+      --address "$ADDRESS" \
+      --log-file client_logs/client-"$i" &
+  pid=$!
+  echo "$pid running"
+  pids+=("$pid")
 done
+
+wait
