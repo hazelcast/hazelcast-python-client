cc8036620fe6368ab7ef6edfdddeea33f5847555
Yuce Tekol <yucetekol@gmail.com>
2026-02-23 11:25:11 +0300

    Port same List tests to asyncio

--- tests/integration/asyncio/proxy/list_test.py
+++ tests/integration/asyncio/proxy/list_test.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from hazelcast.internal.asyncio_proxy.base import ItemEventType
 from tests.integration.asyncio.base import SingleMemberTestCase
 from tests.util import random_string, event_collector
@@ -19,71 +17,7 @@ class ListTest(SingleMemberTestCase):
         await self.list.destroy()
         await super().asyncTearDown()
 
-    async def fill_list(self, items=None):
-        if items is None:
-            items = ["item-%d" % i for i in range(5)]
-        for item in items:
-            await self.list.add(item)
-        return items
-
-    async def test_add(self):
-        result = await self.list.add("item")
-        self.assertTrue(result)
-        self.assertEqual(await self.list.get(0), "item")
-
-    async def test_add_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.add(None)
-
-    async def test_add_appends_to_end(self):
-        await self.list.add("first")
-        await self.list.add("second")
-        self.assertEqual(await self.list.get(1), "second")
-
-    async def test_add_at(self):
-        await self.list.add("a")
-        await self.list.add("c")
-        await self.list.add_at(1, "b")
-        self.assertEqual(await self.list.get(1), "b")
-        self.assertEqual(await self.list.get(2), "c")
-
-    async def test_add_at_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.add_at(0, None)
-
-    async def test_add_all(self):
-        items = ["1", "2", "3"]
-        result = await self.list.add_all(items)
-        self.assertTrue(result)
-        self.assertEqual(await self.list.get(0), "1")
-        self.assertEqual(await self.list.get(1), "2")
-        self.assertEqual(await self.list.get(2), "3")
-
-    async def test_add_all_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.add_all(["1", None, "3"])
-
-    async def test_add_all_null_items(self):
-        with self.assertRaises(AssertionError):
-            await self.list.add_all(None)
-
-    async def test_add_all_at(self):
-        await self.list.add("0")
-        items = ["1", "2", "3"]
-        result = await self.list.add_all_at(1, items)
-        self.assertTrue(result)
-        tail = await self.list.list_iterator(1)
-        self.assertCountEqual(tail, items)
-
-    async def test_add_all_at_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.add_all_at(0, ["1", None])
-
-    async def test_add_all_at_null_items(self):
-        with self.assertRaises(AssertionError):
-            await self.list.add_all_at(0, None)
-
-    async def test_add_listener_item_added(self):
+    async def test_add_entry_listener_item_added(self):
         collector = event_collector()
         await self.list.add_listener(include_value=False, item_added_func=collector)
         await self.list.add("item-value")
@@ -91,12 +25,12 @@ class ListTest(SingleMemberTestCase):
         def assert_event():
             self.assertEqual(len(collector.events), 1)
             event = collector.events[0]
-            self.assertIsNone(event.item)
+            self.assertEqual(event.item, None)
             self.assertEqual(event.event_type, ItemEventType.ADDED)
 
         await self.assertTrueEventually(assert_event, 5)
 
-    async def test_add_listener_item_added_include_value(self):
+    async def test_add_entry_listener_item_added_include_value(self):
         collector = event_collector()
         await self.list.add_listener(include_value=True, item_added_func=collector)
         await self.list.add("item-value")
@@ -109,7 +43,7 @@ class ListTest(SingleMemberTestCase):
 
         await self.assertTrueEventually(assert_event, 5)
 
-    async def test_add_listener_item_removed(self):
+    async def test_add_entry_listener_item_removed(self):
         collector = event_collector()
         await self.list.add_listener(include_value=False, item_removed_func=collector)
         await self.list.add("item-value")
@@ -118,12 +52,12 @@ class ListTest(SingleMemberTestCase):
         def assert_event():
             self.assertEqual(len(collector.events), 1)
             event = collector.events[0]
-            self.assertIsNone(event.item)
+            self.assertEqual(event.item, None)
             self.assertEqual(event.event_type, ItemEventType.REMOVED)
 
         await self.assertTrueEventually(assert_event, 5)
 
-    async def test_add_listener_item_removed_include_value(self):
+    async def test_add_entry_listener_item_removed_include_value(self):
         collector = event_collector()
         await self.list.add_listener(include_value=True, item_removed_func=collector)
         await self.list.add("item-value")
@@ -137,180 +71,192 @@ class ListTest(SingleMemberTestCase):
 
         await self.assertTrueEventually(assert_event, 5)
 
-    async def test_remove_listener(self):
+    async def test_remove_entry_listener_item_added(self):
         collector = event_collector()
         reg_id = await self.list.add_listener(include_value=False, item_added_func=collector)
         await self.list.remove_listener(reg_id)
         await self.list.add("item-value")
 
-        await asyncio.sleep(1)
-        self.assertEqual(len(collector.events), 0)
+        def assert_event():
+            self.assertEqual(len(collector.events), 0)
+            if len(collector.events) > 0:
+                event = collector.events[0]
+                self.assertEqual(event.item, None)
+                self.assertEqual(event.event_type, ItemEventType.ADDED)
+
+        await self.assertTrueEventually(assert_event, 5)
+
+    async def test_add(self):
+        add_resp = await self.list.add("Test")
+        result = await self.list.get(0)
+        self.assertTrue(add_resp)
+        self.assertEqual(result, "Test")
+
+    async def test_add_null_element(self):
+        with self.assertRaises(AssertionError):
+            await self.list.add(None)
+
+    async def test_add_at(self):
+        await self.list.add_at(0, "Test0")
+        await self.list.add_at(1, "Test1")
+        result = await self.list.get(1)
+        self.assertEqual(result, "Test1")
+
+    async def test_add_at_null_element(self):
+        with self.assertRaises(AssertionError):
+            await self.list.add_at(0, None)
+
+    async def test_add_all(self):
+        _all = ["1", "2", "3"]
+        add_resp = await self.list.add_all(_all)
+        result0 = await self.list.get(0)
+        result1 = await self.list.get(1)
+        result2 = await self.list.get(2)
+        self.assertTrue(add_resp)
+        self.assertEqual(result0, "1")
+        self.assertEqual(result1, "2")
+        self.assertEqual(result2, "3")
+
+    async def test_add_all_null_element(self):
+        _all = ["1", "2", "3", None]
+        with self.assertRaises(AssertionError):
+            await self.list.add_all(_all)
+
+    async def test_add_all_null_elements(self):
+        with self.assertRaises(AssertionError):
+            await self.list.add_all(None)
+
+    async def test_add_all_at(self):
+        await self.list.add_at(0, "0")
+        _all = ["1", "2", "3"]
+        add_resp = await self.list.add_all_at(1, _all)
+        _all_resp = await self.list.list_iterator(1)
+        self.assertTrue(add_resp)
+        self.assertCountEqual(_all, _all_resp)
+
+    async def test_add_all_at_null_element(self):
+        _all = ["1", "2", "3", None]
+        with self.assertRaises(AssertionError):
+            await self.list.add_all_at(0, _all)
+
+    async def test_add_all_at_null_elements(self):
+        with self.assertRaises(AssertionError):
+            await self.list.add_all_at(0, None)
 
     async def test_clear(self):
-        await self.fill_list()
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        size = await self.list.size()
         await self.list.clear()
-        self.assertEqual(await self.list.size(), 0)
+        size_cleared = await self.list.size()
+        self.assertEqual(size, len(_all))
+        self.assertEqual(size_cleared, 0)
 
-    async def test_contains_when_present(self):
-        await self.fill_list(["a", "b", "c"])
-        self.assertTrue(await self.list.contains("b"))
+    async def test_contains(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        contains_result = await self.list.contains("2")
+        self.assertTrue(contains_result)
 
-    async def test_contains_when_missing(self):
-        await self.fill_list(["a", "b", "c"])
-        self.assertFalse(await self.list.contains("z"))
-
-    async def test_contains_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.contains(None)
-
-    async def test_contains_all_when_present(self):
-        items = ["1", "2", "3"]
-        await self.fill_list(items)
-        self.assertTrue(await self.list.contains_all(items))
-
-    async def test_contains_all_when_partial(self):
-        await self.fill_list(["1", "2", "3"])
-        self.assertFalse(await self.list.contains_all(["1", "2", "99"]))
-
-    async def test_contains_all_null_items(self):
-        with self.assertRaises(AssertionError):
-            await self.list.contains_all(None)
-
-    async def test_get(self):
-        await self.list.add("hello")
-        self.assertEqual(await self.list.get(0), "hello")
+    async def test_contains_all(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        contains_result = await self.list.contains_all(_all)
+        self.assertTrue(contains_result)
 
     async def test_get_all(self):
-        items = ["1", "2", "3"]
-        await self.fill_list(items)
-        self.assertEqual(await self.list.get_all(), items)
-
-    async def test_get_all_empty(self):
-        self.assertEqual(await self.list.get_all(), [])
-
-    async def test_iterator(self):
-        items = ["1", "2", "3"]
-        await self.fill_list(items)
-        result = await self.list.iterator()
-        self.assertEqual(result, items)
-
-    async def test_index_of(self):
-        await self.fill_list(["a", "b", "c"])
-        self.assertEqual(await self.list.index_of("b"), 1)
-
-    async def test_index_of_not_found(self):
-        await self.fill_list(["a", "b", "c"])
-        self.assertEqual(await self.list.index_of("z"), -1)
-
-    async def test_index_of_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.index_of(None)
-
-    async def test_is_empty_when_empty(self):
-        self.assertTrue(await self.list.is_empty())
-
-    async def test_is_empty_when_not_empty(self):
-        await self.list.add("x")
-        self.assertFalse(await self.list.is_empty())
-
-    async def test_last_index_of(self):
-        await self.fill_list(["1", "2", "2", "3"])
-        self.assertEqual(await self.list.last_index_of("2"), 2)
-
-    async def test_last_index_of_not_found(self):
-        await self.fill_list(["1", "2", "3"])
-        self.assertEqual(await self.list.last_index_of("z"), -1)
-
-    async def test_last_index_of_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.last_index_of(None)
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        all_result = await self.list.get_all()
+        self.assertEqual(all_result, _all)
 
     async def test_list_iterator(self):
-        items = ["1", "2", "3"]
-        await self.fill_list(items)
-        result = await self.list.list_iterator()
-        self.assertEqual(result, items)
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        list_iter = await self.list.list_iterator(1)
+        iter_result = []
+        for item in list_iter:
+            iter_result.append(item)
+        self.assertEqual(iter_result, ["2", "3"])
 
-    async def test_list_iterator_with_index(self):
-        await self.fill_list(["1", "2", "3"])
-        result = await self.list.list_iterator(1)
-        self.assertEqual(result, ["2", "3"])
+    async def test_list_iterator2(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        list_iter = await self.list.list_iterator(1)
+        iter_val = list_iter[1]
+        self.assertEqual(iter_val, "3")
 
-    async def test_remove_existing(self):
-        await self.list.add("item")
-        result = await self.list.remove("item")
-        self.assertTrue(result)
-        self.assertEqual(await self.list.size(), 0)
+    async def test_iterator(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        list_iter = await self.list.iterator()
+        iter_result = []
+        for item in list_iter:
+            iter_result.append(item)
+        self.assertEqual(iter_result, _all)
 
-    async def test_remove_non_existing(self):
-        result = await self.list.remove("no-such-item")
-        self.assertFalse(result)
+    async def test_index_of(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        idx = await self.list.index_of("2")
+        self.assertEqual(idx, 1)
 
-    async def test_remove_null_element(self):
-        with self.assertRaises(AssertionError):
-            await self.list.remove(None)
+    async def test_is_empty(self):
+        is_empty = await self.list.is_empty()
+        self.assertTrue(is_empty)
+
+    async def test_last_index_of(self):
+        _all = ["1", "2", "2", "3"]
+        await self.list.add_all(_all)
+        idx = await self.list.last_index_of("2")
+        self.assertEqual(idx, 2)
+
+    async def test_remove(self):
+        await self.list.add("Test")
+        remove_result = await self.list.remove("Test")
+        size = await self.list.size()
+        self.assertTrue(remove_result)
+        self.assertEqual(size, 0)
 
     async def test_remove_at(self):
-        await self.list.add("item")
-        removed = await self.list.remove_at(0)
-        self.assertEqual(removed, "item")
-        self.assertEqual(await self.list.size(), 0)
-
-    async def test_remove_at_returns_previous_element(self):
-        await self.fill_list(["a", "b", "c"])
-        removed = await self.list.remove_at(1)
-        self.assertEqual(removed, "b")
-        self.assertEqual(await self.list.size(), 2)
+        await self.list.add("Test")
+        remove_result = await self.list.remove_at(0)
+        size = await self.list.size()
+        self.assertTrue(remove_result)
+        self.assertEqual(size, 0)
 
     async def test_remove_all(self):
-        await self.fill_list(["1", "2", "3"])
-        result = await self.list.remove_all(["2", "3"])
-        self.assertTrue(result)
-        self.assertEqual(await self.list.get_all(), ["1"])
-
-    async def test_remove_all_null_items(self):
-        with self.assertRaises(AssertionError):
-            await self.list.remove_all(None)
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        await self.list.remove_all(["2", "3"])
+        result = await self.list.get_all()
+        self.assertEqual(result, ["1"])
 
     async def test_retain_all(self):
-        await self.fill_list(["1", "2", "3"])
-        result = await self.list.retain_all(["2", "3"])
-        self.assertTrue(result)
-        self.assertEqual(await self.list.get_all(), ["2", "3"])
-
-    async def test_retain_all_null_items(self):
-        with self.assertRaises(AssertionError):
-            await self.list.retain_all(None)
-
-    async def test_size(self):
-        items = ["1", "2", "3"]
-        await self.fill_list(items)
-        self.assertEqual(await self.list.size(), len(items))
-
-    async def test_size_empty(self):
-        self.assertEqual(await self.list.size(), 0)
-
-    async def test_set_at(self):
-        await self.fill_list(["1", "2", "3"])
-        previous = await self.list.set_at(1, "22")
-        self.assertEqual(previous, "2")
-        self.assertEqual(await self.list.get(1), "22")
-
-    async def test_set_at_null_element(self):
-        await self.list.add("item")
-        with self.assertRaises(AssertionError):
-            await self.list.set_at(0, None)
-
-    async def test_sub_list(self):
-        await self.fill_list(["1", "2", "3", "4"])
-        result = await self.list.sub_list(1, 3)
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        await self.list.retain_all(["2", "3"])
+        result = await self.list.get_all()
         self.assertEqual(result, ["2", "3"])
 
-    async def test_sub_list_full_range(self):
-        items = ["a", "b", "c"]
-        await self.fill_list(items)
-        result = await self.list.sub_list(0, 3)
-        self.assertEqual(result, items)
+    async def test_size(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        size = await self.list.size()
+        self.assertEqual(size, len(_all))
+
+    async def test_set_at(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        await self.list.set_at(1, "22")
+        result = await self.list.get(1)
+        self.assertEqual(result, "22")
+
+    async def test_sub_list(self):
+        _all = ["1", "2", "3"]
+        await self.list.add_all(_all)
+        sub_list = await self.list.sub_list(1, 3)
+        self.assertEqual(sub_list, ["2", "3"])
 
     def test_str(self):
         self.assertTrue(str(self.list).startswith("List"))
