b44040de8fb08e0900bf952c898b5ed8f795fef3
Yuce Tekol <yucetekol@gmail.com>
2026-03-23 15:29:02 +0300

    Ported Executor to asyncio

--- hazelcast/internal/asyncio_proxy/manager.py
+++ hazelcast/internal/asyncio_proxy/manager.py
@@ -1,9 +1,9 @@
 import asyncio
 import typing
 
+from hazelcast.internal.asyncio_proxy.executor import create_executor_proxy
 from hazelcast.internal.asyncio_proxy.list import create_list_proxy
 from hazelcast.internal.asyncio_proxy.vector_collection import (
-    VectorCollection,
     create_vector_collection_proxy,
 )
 from hazelcast.protocol.codec import client_create_proxy_codec, client_destroy_proxy_codec
@@ -13,6 +13,7 @@ from hazelcast.internal.asyncio_proxy.map import create_map_proxy
 from hazelcast.internal.asyncio_proxy.replicated_map import create_replicated_map_proxy
 from hazelcast.util import to_list
 
+EXECUTOR_SERVICE = "hz:impl:executorService"
 LIST_SERVICE = "hz:impl:listService"
 MAP_SERVICE = "hz:impl:mapService"
 REPLICATED_MAP_SERVICE = "hz:impl:replicatedMapService"
@@ -22,6 +23,7 @@ _proxy_init: typing.Dict[
     str,
     typing.Callable[[str, str, typing.Any], typing.Coroutine[typing.Any, typing.Any, typing.Any]],
 ] = {
+    EXECUTOR_SERVICE: create_executor_proxy,
     LIST_SERVICE: create_list_proxy,
     MAP_SERVICE: create_map_proxy,
     REPLICATED_MAP_SERVICE: create_replicated_map_proxy,
