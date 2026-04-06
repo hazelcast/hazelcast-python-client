94e769fefdd2d4ada80f7197094ed96a17d91a45
Yüce Tekol <yuce.tekol@hazelcast.com>
2026-04-06 15:22:14 +0300

    [HZ-5403] Queue for Asyncio Client (#788)
    Straightforward  port of Queue and its tests to asyncio client.

--- hazelcast/internal/asyncio_proxy/manager.py
+++ hazelcast/internal/asyncio_proxy/manager.py
@@ -3,6 +3,7 @@ import typing
 
 from hazelcast.internal.asyncio_proxy.list import create_list_proxy
 from hazelcast.internal.asyncio_proxy.multi_map import create_multi_map_proxy
+from hazelcast.internal.asyncio_proxy.queue import create_queue_proxy
 from hazelcast.internal.asyncio_proxy.vector_collection import (
     VectorCollection,
     create_vector_collection_proxy,
@@ -17,6 +18,7 @@ from hazelcast.util import to_list
 LIST_SERVICE = "hz:impl:listService"
 MAP_SERVICE = "hz:impl:mapService"
 MULTI_MAP_SERVICE = "hz:impl:multiMapService"
+QUEUE_SERVICE = "hz:impl:queueService"
 REPLICATED_MAP_SERVICE = "hz:impl:replicatedMapService"
 VECTOR_SERVICE = "hz:service:vector"
 
@@ -27,6 +29,7 @@ _proxy_init: typing.Dict[
     LIST_SERVICE: create_list_proxy,
     MAP_SERVICE: create_map_proxy,
     MULTI_MAP_SERVICE: create_multi_map_proxy,
+    QUEUE_SERVICE: create_queue_proxy,
     REPLICATED_MAP_SERVICE: create_replicated_map_proxy,
     VECTOR_SERVICE: create_vector_collection_proxy,
 }
